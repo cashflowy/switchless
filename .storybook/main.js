f9f7eed72ddc7cb8f929af6b1f7e3ed1fa8fcaf8
Alex J V <alexjv89@gmail.com>
2024-08-23 16:59:31 +0530

    code addon added

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,6 +13,7 @@ const config = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-storysource",
   ],
   framework: {
     name: "@storybook/react-webpack5",
