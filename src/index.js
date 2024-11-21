20e789fcf7c301b2135d9c55331b5422e01d07c8
Alex J V <alexjv89@gmail.com>
2024-11-21 11:17:47 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,38 +1,34 @@
 // Page components
-export { Table } from './styled/page/Table/Table';
-export { PageHeader } from './styled/page/PageHeader/PageHeader';
-export { CustomTab } from './styled/page/CustomTab/CustomTab';
-export { Logo } from './styled/page/Logo/Logo';
-export { ClickToEdit } from './styled/page/ClickToEdit/ClickToEdit';
-export { TabNavigation } from './styled/page/TabNavigation/TabNavigation';
-export { Pagination } from './styled/page/Pagination/Pagination';
-export { Login } from './styled/page/Login/Login';
-export { CustomStepper } from './styled/page/Stepper/CustomStepper';
-export { EmptyState } from './styled/page/EmptyState/EmptyState';
-export { CustomSnackbar } from './styled/page/CustomSnackbar/CustomSnackbar';
+export { default as Logo } from './styled/page/Logo/Logo';
+export { default as Table } from './styled/page/Table/Table';
+export { default as PageHeader } from './styled/page/PageHeader/PageHeader';
+export { default as CustomTab } from './styled/page/CustomTab/CustomTab';
+export { default as ClickToEdit } from './styled/page/ClickToEdit/ClickToEdit';
+export { default as TabNavigation } from './styled/page/TabNavigation/TabNavigation';
+export { default as Pagination } from './styled/page/Pagination/Pagination';
+export { default as Login } from './styled/page/Login/Login';
+export { default as CustomStepper } from './styled/page/Stepper/CustomStepper';
+export { default as EmptyState } from './styled/page/EmptyState/EmptyState';
+export { default as CustomSnackbar } from './styled/page/CustomSnackbar/CustomSnackbar';
 
 // Date components
-export { Date } from './styled/date/Date/Date';
-export { DateTime } from './styled/date/DateTime/DateTime';
+export { default as Date } from './styled/date/Date/Date';
+export { default as DateTime } from './styled/date/DateTime/DateTime';
 
 // Currency components
-export { CurrencyFormatter } from './styled/currency/CurrencyFormatter/CurrencyFormatter';
+export { default as CurrencyFormatter } from './styled/currency/CurrencyFormatter/CurrencyFormatter';
 
 // Other styled components
-export { TableOrCards } from './styled/others/TableOrCards/TableOrCards';
+export { default as TableOrCards } from './styled/others/TableOrCards/TableOrCards';
 
 // Primitive components
-export { Link } from './primitive/Link/Link';
-export { TextField } from './primitive/TextField/TextField';
+export { default as Link } from './primitive/Link/Link';
+export { default as TextField } from './primitive/TextField/TextField';
 
 // Utility components
-export { MetricCard } from './utility/MetricCard/MetricCard';
-export { NumberFormatter } from './utility/NumberFormatter/NumberFormatter';
-export { LoomIframe } from './utility/LoomIframe/LoomIframe';
-export { CommentCard } from './utility/CommentCard/CommentCard';
-export { MoreOptionsMenu } from './utility/MoreOptionsMenu/MoreOptionsMenu';
-
-// Type definitions (if you have any)
-// export type { TableProps } from './styled/page/Table/Table';
-// ... add other type exports as needed
+export { default as MetricCard } from './utility/MetricCard/MetricCard';
+export { default as NumberFormatter } from './utility/NumberFormatter/NumberFormatter';
+export { default as LoomIframe } from './utility/LoomIframe/LoomIframe';
+export { default as CommentCard } from './utility/CommentCard/CommentCard';
+export { default as MoreOptionsMenu } from './utility/MoreOptionsMenu/MoreOptionsMenu';
 
