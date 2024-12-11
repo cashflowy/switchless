ecd12b52794225aa5483bb745c2338145f2766ec
Fahim P Ansari <fahimpjan@gmail.com>
2024-12-11 16:46:24 +0530

    Bug fix in CompactTable component

--- src/styled/page/CompactTable/CompactTable.jsx
+++ src/styled/page/CompactTable/CompactTable.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import Sheet from '@mui/joy/Sheet';
 import {Table as MuiTable} from '@mui/joy';
-import PropTypes from 'prop-types';
 
-/** 
- * Custom defined table. 
- * - is scrollable 
- * - is plain and simple
- */
-export default function Table({type='topHeader',borderAxis,sx={}, children, hoverRow=false, variant='outlined', stickyHeader=false}){
+
+export default function CompactTable({type='topHeader',borderAxis,sx={}, children, hoverRow=false, variant='outlined', stickyHeader=false}){
 	var tableSX={
 		...sx,
 		'& tbody': { bgcolor: 'white' },
@@ -43,18 +38,3 @@ export default function Table({type='topHeader',borderAxis,sx={}, children, hove
 	)
 }
 
-
-
-
-Table.propTypes = {
-	/**
-	 * There are 2 types of table, Top header and Side header
-	 */
-	type:PropTypes.oneOf(['topHeader','sideHeader']),
-	/**
-	 * The content of the table
-	 */
-	children:PropTypes.node,
-	borderAxis: PropTypes.oneOf(['x', 'y', 'both', 'none', 'xBetween', 'yBetween', 'bothBetween']),
-	sx: PropTypes.object,
-};
--- src/styled/page/CompactTable/CompactTable.stories.js
+++ src/styled/page/CompactTable/CompactTable.stories.js
@@ -13,7 +13,6 @@ export default {
 };
 
 
-
 import * as React from 'react';
 
 
