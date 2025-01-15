a61498cb27836677f55c2fb62ae46432ce3d14a1
Fahim P Ansari <fahimpjan@gmail.com>
2025-01-15 19:40:18 +0530

    build issue

---
+++ _webpack.config.js
@@ -0,0 +1,21 @@
+
+export default {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
