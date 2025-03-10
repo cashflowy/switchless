15df124c637b81aa5719aef42d6b091150a56ec9
Fahim P Ansari <fahimpjan@gmail.com>
2025-03-10 13:38:21 +0530

    package issue fixed

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 // Page components
-export { default as Logo } from './styled/page/Logo/Logo';
-export { default as Table } from './styled/page/Table/Table';
-export { default as CompactTable } from './styled/page/CompactTable/CompactTable';
-export { default as PageHeader } from './styled/page/PageHeader/PageHeader';
-export { default as CustomTab } from './styled/page/CustomTab/CustomTab';
-export { default as ClickToEdit } from './styled/page/ClickToEdit/ClickToEdit';
-export { default as TabNavigation } from './styled/page/TabNavigation/TabNavigation';
-export { default as Pagination } from './styled/page/Pagination/Pagination';
-export { default as Pagination2 } from './styled/page/Pagination2/Pagination2';
-export { default as StatsBarContainer} from './styled/page/StatsBarContainer/StatsBarContainer';
-export {default as FoldFilterContainer} from './styled/page/FoldFilterContainer/FoldFilterContainer';
-export {default as SideFilter} from './styled/form/SideFilter/SideFilter';
-export { default as EmptyState } from './styled/page/EmptyState/EmptyState';
-export { default as ShowJSON } from './styled/page/ShowJSON/ShowJSON';
-export { default as AlertBox } from './styled/page/AlertBox/AlertBox';
-export { default as VerticalMenu } from './styled/page/VerticalMenu/VerticalMenu';
+export { default as Logo } from './styled/page/Logo/Logo.jsx';
+export { default as Table } from './styled/page/Table/Table.jsx';
+export { default as CompactTable } from './styled/page/CompactTable/CompactTable.jsx';
+export { default as PageHeader } from './styled/page/PageHeader/PageHeader.jsx';
+export { default as CustomTab } from './styled/page/CustomTab/CustomTab.jsx';
+export { default as ClickToEdit } from './styled/page/ClickToEdit/ClickToEdit.jsx';
+export { default as TabNavigation } from './styled/page/TabNavigation/TabNavigation.jsx';
+export { default as Pagination } from './styled/page/Pagination/Pagination.jsx';
+export { default as Pagination2 } from './styled/page/Pagination2/Pagination2.jsx';
+export { default as StatsBarContainer} from './styled/page/StatsBarContainer/StatsBarContainer.jsx';
+export {default as FoldFilterContainer} from './styled/page/FoldFilterContainer/FoldFilterContainer.jsx';
+// export {default as SideFilter} from './styled/form/SideFilter/SideFilter.jsx';
+export { default as EmptyState } from './styled/page/EmptyState/EmptyState.jsx';
+export { default as ShowJSON } from './styled/page/ShowJSON/ShowJSON.jsx';
+export { default as AlertBox } from './styled/page/AlertBox/AlertBox.jsx';
+export { default as VerticalMenu } from './styled/page/VerticalMenu/VerticalMenu.jsx';
 
 // Date components
-export { default as Date } from './styled/date/Date/Date';
-export { default as DateTime } from './styled/date/DateTime/DateTime';
+export { default as Date } from './styled/date/Date/Date.jsx';
+export { default as DateTime } from './styled/date/DateTime/DateTime.jsx';
 
 // Currency components
-export { default as CurrencyFormatter } from './styled/currency/CurrencyFormatter/CurrencyFormatter';
+export { default as CurrencyFormatter } from './styled/currency/CurrencyFormatter/CurrencyFormatter.jsx';
 
 // Other styled components
-export { default as TableOrCards } from './styled/others/TableOrCards/TableOrCards';
-export { default as TopLoader } from './styled/others/TopLoader/TopLoader';
+export { default as TableOrCards } from './styled/others/TableOrCards/TableOrCards.jsx';
+export { default as TopLoader } from './styled/others/TopLoader/TopLoader.jsx';
 
 // Primitive components
-export { default as Link } from './primitive/Link/Link';
-export { default as TextField } from './primitive/TextField/TextField';
+export { default as Link } from './primitive/Link/Link.jsx';
+export { default as TextField } from './primitive/TextField/TextField.jsx';
 
 // Utility components
-export { default as MetricCard } from './utility/MetricCard/MetricCard';
-export { default as NumberFormatter } from './utility/NumberFormatter/NumberFormatter';
-export { default as LoomIframe } from './utility/LoomIframe/LoomIframe';
-export { default as CommentCard } from './utility/CommentCard/CommentCard';
+export { default as MetricCard } from './utility/MetricCard/MetricCard.jsx';
+export { default as NumberFormatter } from './utility/NumberFormatter/NumberFormatter.jsx';
+export { default as LoomIframe } from './utility/LoomIframe/LoomIframe.jsx';
+export { default as CommentCard } from './utility/CommentCard/CommentCard.jsx';
 
--- src/primitive/TextField/TextField.jsx
+++ src/primitive/TextField/TextField.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react';
-import {Input,FormLabel, FormControl, FormHelperText} from '@mui/joy';
+import {Input,FormLabel, FormControl } from '@mui/joy';
 // import { FormControl } from '@mui/base/FormControl';
 export default function TextField({name,defaultValue,placeholder,label,required,type,step,endDecorator,startDecorator,disabled,hidden,onBlur}){
 	var number_sx = {
